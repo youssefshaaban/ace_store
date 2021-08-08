e2930993066110d302482806ac787b272706f9ac
khabeer <yousefshaaban0@gmail.com>
2021-08-09 00:04:51 +0200

    add new view rateMe and detail Item

---
+++ app/src/main/java/com/example/mvvm_template/ui/component/main/rate_app/RateMeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mvvm_template.ui.component.main.rate_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvvm_template.R
+
+class RateMeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rate_me)
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,8 @@ buildscript {
 
     classpath(Dependencies.AndroidGradlePlugin)
     classpath(Dependencies.KotlinPlugin)
-   // classpath(Dependencies.HiltClathPath)
+      classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+      // classpath(Dependencies.HiltClathPath)
       // NOTE: Do not place your application dependencies here; they belong
     // in the individual module build.gradle.kts.kts files
   }
