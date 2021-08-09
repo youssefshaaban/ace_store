f1a6e64e89257443b557ebbfb03ec4786a9932fa
khabeer <yousefshaaban0@gmail.com>
2021-08-09 23:50:06 +0200

    add new view rateMe and detail Item

---
+++ app/src/main/java/com/example/mvvm_template/ui/component/reorder/ReorderActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mvvm_template.ui.component.reorder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvvm_template.R
+
+class ReorderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reorder)
+    }
+}
